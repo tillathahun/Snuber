9286bd7177ce9ccabc0e1f4c17db5bdc12d26d81
Myles Spencer <mespencer@wpi.edu>
2017-02-26 14:13:14 -0500

    Registered ride request and core apps with project.

--- server/snuber/settings.py
+++ server/snuber/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
+    'ride_requests.apps.RideRequestsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
