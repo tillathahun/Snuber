a6653c446620a8911077d72b11b4094cdb2917ac
Myles Spencer <mespencer@wpi.edu>
2017-02-26 14:10:46 -0500

    Registered core and ride_requests urls to the root urls file

--- server/snuber/urls.py
+++ server/snuber/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('core.urls')),
+    url(r'^ride-requests/', include('ride_rquests.urls'))
     url(r'^admin/', admin.site.urls),
 ]
