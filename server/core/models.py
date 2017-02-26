82079af7a93d7152382eb9a4904f11a6c6c33c37
Myles Spencer <mespencer@wpi.edu>
2017-02-25 22:45:02 -0500

    Added user model and api files

---
+++ server/core/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='avatar')
+    latitude = models.DecimalField(max_digits=7, decimal_places=5)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5)
