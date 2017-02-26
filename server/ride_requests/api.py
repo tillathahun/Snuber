3dc0b2d2d5aa4f39547eff8e1ad0e38144349bbf
Myles Spencer <mespencer@wpi.edu>
2017-02-26 14:03:43 -0500

    Added ride requeseting to api

--- server/ride_requests/api.py
+++ server/ride_requests/api.py
@@ -4,6 +4,17 @@ from django.shortcuts import get_object_or_404
 from .models import RideRequest
 
 @login_required
+def request_ride(request):
+    if request.method != 'POST':
+        HttpResponseNotFound('Incorrect access method')
+
+    dest_latitude = request.POST.get('destination_latitude')
+    dest_longitude = request.POST.get('destination_longitude')
+    ride = RideRequest(user=request.user, destination_latitude=dest_latitude, destination_longitude=dest_longitude)
+    ride.save()
+    return JsonResponse({'success': True})
+
+@login_required
 def cancel_ride_request(request, id):
     ride = get_object_or_404(RideRequest, pk=id)
     if ride.user = request.user:
