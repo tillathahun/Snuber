a028aeb80632acbc79801b03af8e7b9cb8bfffde
Myles Spencer <mespencer@wpi.edu>
2017-02-26 14:45:54 -0500

    Updated the user model to allow latitudes and longitudes to be NULL in the database

--- server/core/models.py
+++ server/core/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatar')
-    latitude = models.DecimalField(max_digits=7, decimal_places=5)
-    longitude = models.DecimalField(max_digits=8, decimal_places=5)
+    latitude = models.DecimalField(max_digits=7, decimal_places=5, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5, blank=True, null=True)
     isDriver = models.BooleanField(default=False)
