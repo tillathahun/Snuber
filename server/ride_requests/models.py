94eaf60a081c8ed48f0536684a8da60df1a170da
Myles Spencer <mespencer@wpi.edu>
2017-02-25 22:54:21 -0500

    Added first rev of RideRequest model

--- server/ride_requests/models.py
+++ server/ride_requests/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class RideRequest(models.Model):
+    destination_latitude = models.DecimalField(max_digits=7, decimal_places=5)
+    destination_longitude = models.DecimalField(max_digits=8, decimal_places=5)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='ride_requests')
+    driver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='rides_requested')
