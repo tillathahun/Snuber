4d71700a85c121dd4f72d278cd3b3f17aeaad627
Myles Spencer <mespencer@wpi.edu>
2017-02-25 22:54:02 -0500

    Added isDriver to user model

--- server/core/models.py
+++ server/core/models.py
@@ -5,3 +5,4 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatar')
     latitude = models.DecimalField(max_digits=7, decimal_places=5)
     longitude = models.DecimalField(max_digits=8, decimal_places=5)
+    isDriver = models.BooleanField(default=False)
