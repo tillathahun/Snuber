321f131e57bd62c784681a2deaf68f5d81e8a49f
Myles Spencer <mespencer@wpi.edu>
2017-02-28 11:40:42 -0500

    Added backslash in update-location url

--- server/core/urls.py
+++ server/core/urls.py
@@ -5,5 +5,5 @@ from . import api
 urlpatterns = [
     url(r'^api/register/', api.register, name='api_register'),
     url(r'^api/login/', api.login, name='api_login'),
-    url(r'^api/update-location', api.update_location, name='api_update_location')
+    url(r'^api/update-location/', api.update_location, name='api_update_location')
 ]
