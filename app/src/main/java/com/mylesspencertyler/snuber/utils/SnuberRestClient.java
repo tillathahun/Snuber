86bfd7f75c142ca7fe3387545abc9713011b6c23
Myles Spencer <mspencer.mes@gmail.com>
2017-02-28 11:31:09 -0500

    Added persistent cookie storage

--- app/src/main/java/com/mylesspencertyler/snuber/utils/SnuberRestClient.java
+++ app/src/main/java/com/mylesspencertyler/snuber/utils/SnuberRestClient.java
@@ -4,12 +4,19 @@ package com.mylesspencertyler.snuber.utils;
  * Created by mspen on 2/27/2017.
  */
 
+import android.content.Context;
+
 import com.loopj.android.http.*;
 
 public class SnuberRestClient {
     private static final String BASE_URL = "http://192.168.1.100:8000/";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void setup(Context context) {
+        PersistentCookieStore store = new PersistentCookieStore(context);
+        SnuberRestClient.client.setCookieStore(store);
+    }
 
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.get(getAbsoluteUrl(url), params, responseHandler);
