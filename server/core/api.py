ca1582e367242e0acd810f1b12554f3971cf1ddb
Myles Spencer <mespencer@wpi.edu>
2017-02-28 21:44:12 -0500

    Removed debug message in login

--- server/core/api.py
+++ server/core/api.py
@@ -23,7 +23,7 @@ def login(request):
 
     username = request.POST.get('username')
     password = request.POST.get('password')
-    print(username, password)
+    
     user = authenticate(username=username, password=password)
     if user is not None:
         login(request, user)
