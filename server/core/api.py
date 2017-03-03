699f54b55ecd3ffd9848c469095a8276d5b9155e
Myles Spencer <mespencer@wpi.edu>
2017-03-03 05:09:31 -0500

    Imported Sys

--- server/core/api.py
+++ server/core/api.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from .forms import UserForm
 from django.middleware.csrf import get_token
+import sys
 sys.path.insert(0, '../ride_requests')
 from api import update_ride
 
