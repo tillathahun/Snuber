a800d6ed10bfb5e89c99fae788efb5faccdeafd0
Tyler Bennett <tylerbennett215@gmail.com>
2017-03-02 23:27:17 -0500

    added notification service using firebase to make the app functional

---
+++ app/src/main/java/com/mylesspencertyler/snuber/utils/FirebaseHelper.java
@@ -0,0 +1,27 @@
+package com.mylesspencertyler.snuber.utils;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by tyler on 3/2/2017.
+ */
+
+public class FirebaseHelper extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FirebaseHelper", "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+        // store this in the server, call a snuber client function here that updates the firebase token
+    }
+}
